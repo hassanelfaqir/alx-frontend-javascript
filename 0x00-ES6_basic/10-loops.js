9aaac70ece729ec1c5a5918c86f3811809493514
hassanelfaqir <hassanelfaqir@Linux.myguest.virtualbox.org>
2024-04-23 02:36:52 +0200

    alx-frontend-javascript

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const appendedArray = [];
+  for (const index of array) {
+    const value = index;
+    appendedArray.push(appendString + value);
+  }
+  return appendedArray;
+}
